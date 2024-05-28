6ec140fb7da501c2760680a185c3eca705d15bc3
Naveen <naveendhananjaya@aol.com>
2024-05-28 08:15:53 +0530

    Version 0.2

--- browser.py
+++ browser.py
@@ -1,47 +1,161 @@
-from PyQt5.QtWidgets import QMainWindow, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QLineEdit, QTabWidget, QStatusBar, QVBoxLayout, QWidget, QAction, QToolBar, QDialog, QListWidget
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QUrl
-from ui_components import create_toolbar, create_menu
+from PyQt5.QtCore import QUrl, QSize, Qt
+from PyQt5.QtGui import QIcon, QFont, QPalette, QColor
+from PyQt5.QtPrintSupport import QPrintDialog
+from ui_components import create_toolbar, create_menu, create_download_manager, create_find_toolbar
 
 class Browser(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Enrco")
+        self.setWindowTitle("Modern Web Browser")
         self.setGeometry(100, 100, 1200, 800)
+        
+        self.tabs = QTabWidget()
+        self.tabs.setTabsClosable(True)
+        self.tabs.tabCloseRequested.connect(self.close_current_tab)
+        self.tabs.currentChanged.connect(self.current_tab_changed)
 
-        self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("http://www.google.com"))
-
-        self.setCentralWidget(self.browser)
+        self.setCentralWidget(self.tabs)
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
 
         self.url_bar = QLineEdit()
         self.url_bar.returnPressed.connect(self.navigate_to_url)
-        self.browser.urlChanged.connect(self.update_url)
+        self.url_bar.setFont(QFont("Arial", 12))
 
-        navbar = create_toolbar(self)
-        self.addToolBar(navbar)
+        self.toolbar = create_toolbar(self)
+        self.addToolBar(self.toolbar)
 
         menubar = create_menu(self)
         self.setMenuBar(menubar)
 
-    def back(self):
-        self.browser.back()
+        self.download_manager = create_download_manager(self)
+        self.find_toolbar = create_find_toolbar(self)
 
-    def forward(self):
-        self.browser.forward()
+        self.add_new_tab(QUrl("http://www.google.com"), "Home")
 
-    def reload(self):
-        self.browser.reload()
+    def add_new_tab(self, qurl=None, label="Blank"):
+        if qurl is None:
+            qurl = QUrl("")
 
-    def navigate_home(self):
-        self.browser.setUrl(QUrl("http://www.google.com"))
+        browser = QWebEngineView()
+        browser.setUrl(qurl)
+        browser.urlChanged.connect(lambda q: self.update_url_bar(q, browser))
+        browser.loadFinished.connect(lambda _: self.tabs.setTabText(self.tabs.indexOf(browser), browser.page().title()))
 
-    def navigate_new_page(self):
-        self.browser.setUrl(QUrl("http://www.example.com"))
+        i = self.tabs.addTab(browser, label)
+        self.tabs.setCurrentIndex(i)
+        browser.page().urlChanged.connect(self.save_history)
+
+    def close_current_tab(self, i):
+        if self.tabs.count() < 2:
+            return
+        self.tabs.removeTab(i)
+
+    def current_tab_changed(self, i):
+        qurl = self.tabs.currentWidget().url()
+        self.update_url_bar(qurl, self.tabs.currentWidget())
 
     def navigate_to_url(self):
         url = self.url_bar.text()
-        self.browser.setUrl(QUrl(url))
+        self.tabs.currentWidget().setUrl(QUrl(url))
 
-    def update_url(self, q):
+    def update_url_bar(self, q, browser=None):
+        if browser != self.tabs.currentWidget():
+            return
         self.url_bar.setText(q.toString())
+        self.url_bar.setCursorPosition(0)
+
+    def navigate_home(self):
+        self.tabs.currentWidget().setUrl(QUrl("http://www.google.com"))
+
+    def navigate_back(self):
+        self.tabs.currentWidget().back()
+
+    def navigate_forward(self):
+        self.tabs.currentWidget().forward()
+
+    def reload_page(self):
+        self.tabs.currentWidget().reload()
+
+    def add_bookmark(self):
+        url = self.tabs.currentWidget().url().toString()
+        with open("bookmarks.txt", "a") as f:
+            f.write(url + "\n")
+
+    def show_bookmarks(self):
+        dialog = QDialog()
+        dialog.setWindowTitle("Bookmarks")
+        layout = QVBoxLayout()
+        bookmark_list = QListWidget()
+        layout.addWidget(bookmark_list)
+        dialog.setLayout(layout)
+        
+        try:
+            with open("bookmarks.txt", "r") as f:
+                bookmarks = f.readlines()
+            bookmarks = [x.strip() for x in bookmarks]
+            for bookmark in bookmarks:
+                bookmark_list.addItem(bookmark)
+                bookmark_list.itemClicked.connect(lambda item: self.add_new_tab(QUrl(item.text()), item.text()))
+        except FileNotFoundError:
+            pass
+        
+        dialog.exec_()
+
+    def show_history(self):
+        dialog = QDialog()
+        dialog.setWindowTitle("History")
+        layout = QVBoxLayout()
+        history_list = QListWidget()
+        layout.addWidget(history_list)
+        dialog.setLayout(layout)
+        
+        try:
+            with open("history.txt", "r") as f:
+                history = f.readlines()
+            history = [x.strip() for x in history]
+            for url in history:
+                history_list.addItem(url)
+                history_list.itemClicked.connect(lambda item: self.add_new_tab(QUrl(item.text()), item.text()))
+        except FileNotFoundError:
+            pass
+        
+        dialog.exec_()
+
+    def save_history(self, qurl):
+        with open("history.txt", "a") as f:
+            f.write(qurl.toString() + "\n")
+
+    def find_text(self, text):
+        self.tabs.currentWidget().findText(text)
+
+    def zoom_in(self):
+        self.tabs.currentWidget().setZoomFactor(self.tabs.currentWidget().zoomFactor() + 0.1)
+
+    def zoom_out(self):
+        self.tabs.currentWidget().setZoomFactor(self.tabs.currentWidget().zoomFactor() - 0.1)
+
+    def toggle_dark_mode(self):
+        app.setStyle("Fusion")
+        dark_palette = QPalette()
+        dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.WindowText, Qt.white)
+        dark_palette.setColor(QPalette.Base, QColor(15, 15, 15))
+        dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+        dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+        dark_palette.setColor(QPalette.Text, Qt.white)
+        dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+        dark_palette.setColor(QPalette.ButtonText, Qt.white)
+        dark_palette.setColor(QPalette.BrightText, Qt.red)
+        dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+        dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+        dark_palette.setColor(QPalette.HighlightedText, Qt.black)
+        app.setPalette(dark_palette)
+
+    def print_page(self):
+        dialog = QPrintDialog()
+        if dialog.exec_() == QDialog.Accepted:
+            self.tabs.currentWidget().page().print(dialog.printer(), lambda ok: None)
--- ui_components.py
+++ ui_components.py
@@ -1,29 +1,62 @@
-from PyQt5.QtWidgets import QToolBar, QAction, QMenuBar
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QToolBar, QAction, QMenuBar, QMenu, QLineEdit
+from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QSize
 
 def create_toolbar(browser):
     toolbar = QToolBar()
     toolbar.setIconSize(QSize(24, 24))
 
-    back_btn = QAction(QIcon('icons/back.png'), 'Back', browser)
-    back_btn.triggered.connect(browser.back)
+    back_btn = QAction('◀', browser)
+    back_btn.setFont(QFont("Arial", 16))
+    back_btn.triggered.connect(browser.navigate_back)
     toolbar.addAction(back_btn)
 
-    forward_btn = QAction(QIcon('icons/forward.png'), 'Forward', browser)
-    forward_btn.triggered.connect(browser.forward)
+    forward_btn = QAction('▶', browser)
+    forward_btn.setFont(QFont("Arial", 16))
+    forward_btn.triggered.connect(browser.navigate_forward)
     toolbar.addAction(forward_btn)
 
-    reload_btn = QAction(QIcon('icons/reload.png'), 'Reload', browser)
-    reload_btn.triggered.connect(browser.reload)
+    reload_btn = QAction('↻', browser)
+    reload_btn.setFont(QFont("Arial", 16))
+    reload_btn.triggered.connect(browser.reload_page)
     toolbar.addAction(reload_btn)
 
-    home_btn = QAction(QIcon('icons/home.png'), 'Home', browser)
+    home_btn = QAction('⌂', browser)
+    home_btn.setFont(QFont("Arial", 16))
     home_btn.triggered.connect(browser.navigate_home)
     toolbar.addAction(home_btn)
 
-    toolbar.addSeparator()
+    bookmark_btn = QAction('★', browser)
+    bookmark_btn.setFont(QFont("Arial", 16))
+    bookmark_btn.triggered.connect(browser.add_bookmark)
+    toolbar.addAction(bookmark_btn)
 
+    find_btn = QAction('🔍', browser)
+    find_btn.setFont(QFont("Arial", 16))
+    find_btn.triggered.connect(lambda: browser.find_toolbar.setVisible(not browser.find_toolbar.isVisible()))
+    toolbar.addAction(find_btn)
+
+    zoom_in_btn = QAction('A+', browser)
+    zoom_in_btn.setFont(QFont("Arial", 16))
+    zoom_in_btn.triggered.connect(browser.zoom_in)
+    toolbar.addAction(zoom_in_btn)
+
+    zoom_out_btn = QAction('A-', browser)
+    zoom_out_btn.setFont(QFont("Arial", 16))
+    zoom_out_btn.triggered.connect(browser.zoom_out)
+    toolbar.addAction(zoom_out_btn)
+
+    dark_mode_btn = QAction('🌙', browser)
+    dark_mode_btn.setFont(QFont("Arial", 16))
+    dark_mode_btn.triggered.connect(browser.toggle_dark_mode)
+    toolbar.addAction(dark_mode_btn)
+
+    print_btn = QAction('🖨', browser)
+    print_btn.setFont(QFont("Arial", 16))
+    print_btn.triggered.connect(browser.print_page)
+    toolbar.addAction(print_btn)
+
+    toolbar.addSeparator()
     toolbar.addWidget(browser.url_bar)
 
     return toolbar
@@ -33,8 +66,40 @@ def create_menu(browser):
 
     file_menu = menubar.addMenu('File')
 
-    new_page_action = QAction('New Page', browser)
-    new_page_action.triggered.connect(browser.navigate_new_page)
-    file_menu.addAction(new_page_action)
+    new_tab_action = QAction('New Tab', browser)
+    new_tab_action.triggered.connect(lambda: browser.add_new_tab())
+    file_menu.addAction(new_tab_action)
+
+    bookmarks_menu = menubar.addMenu('Bookmarks')
+
+    show_bookmarks_action = QAction('Show Bookmarks', browser)
+    show_bookmarks_action.triggered.connect(browser.show_bookmarks)
+    bookmarks_menu.addAction(show_bookmarks_action)
+
+    history_menu = menubar.addMenu('History')
+
+    show_history_action = QAction('Show History', browser)
+    show_history_action.triggered.connect(browser.show_history)
+    history_menu.addAction(show_history_action)
 
     return menubar
+
+def create_download_manager(browser):
+    pass
+
+def create_find_toolbar(browser):
+    toolbar = QToolBar()
+    toolbar.setIconSize(QSize(16, 16))
+
+    find_text_box = QLineEdit()
+    find_text_box.setPlaceholderText("Find text...")
+    toolbar.addWidget(find_text_box)
+
+    find_btn = QAction('🔍', browser)
+    find_btn.setFont(QFont("Arial", 16))
+    find_btn.triggered.connect(lambda: browser.find_text(find_text_box.text()))
+    toolbar.addAction(find_btn)
+
+    toolbar.setVisible(False)
+    browser.addToolBar(toolbar)
+    return toolbar
